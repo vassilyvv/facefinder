8d0a7bbd5ab058700ff8e52405336c9670d50a36
zuber <amazingbasil@gmail.com>
2015-05-26 20:55:09 +0600

    1. хэш в имени файла 2. папка db создаётся автоматически 3. захардкодил путь к каскаду

--- facepalm.py
+++ facepalm.py
@@ -1,12 +1,11 @@
 import cv2
 import sys
+import os
 import ImageChops
 from PIL import Image
 from os import listdir
+import hashlib
 
-db_path = '/home/zuber/projects/facepalm/db'
-imagePath = sys.argv[1]
-cascPath = sys.argv[2]
 
 def find_image_in_db(img):
     for db_img_path in listdir(db_path):
@@ -14,7 +13,15 @@ def find_image_in_db(img):
         if not ImageChops.difference(Image.fromarray(db_img), Image.fromarray(img)).getbbox():
             return db_img_path
 
-faceCascade = cv2.CascadeClassifier(cascPath)
+dirpath = '/'.join(os.path.realpath(__file__).split('/')[:-1])
+db_path = dirpath + '/db/'
+imagePath = sys.argv[1]
+cascadePath = 'haarcascade_frontalface_default.xml'
+
+if not os.path.exists(dirpath + '/db/'):
+    os.makedirs(dirpath + '/db/')
+
+faceCascade = cv2.CascadeClassifier(cascadePath)
 
 image = cv2.imread(imagePath)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -27,14 +34,15 @@ faces = faceCascade.detectMultiScale(
     flags = cv2.cv.CV_HAAR_SCALE_IMAGE
 )
 
-# Draw a rectangle around the faces
-#for (x, y, w, h) in faces:
-#    cv2.rectangle(image, (x, y), (x+w, y+h), (255, 255, 0), 2)
 cutted_images = [image[face[1]:face[1] + face[3], face[0]: face[0] + face[2]] for face in faces]
+
 for img in cutted_images:
     search_result = find_image_in_db(img)
     if search_result:
-        print "face found: " + search_result
+        print "Face found: " + search_result
     else:
-        print 'j'
-        cv2.imwrite('db/img%d.png' % (len(listdir(db_path)) + 1), img)
+        m = hashlib.md5()
+        m.update(str(img))
+        new_path = 'db/%s.png' % m.hexdigest()
+        cv2.imwrite(new_path, img)
+        print 'New face in database: %s' % new_path
